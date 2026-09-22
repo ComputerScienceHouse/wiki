53f651e41c5a4c379722409438ff8f1b7f68cdca
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-09-21 23:50:33 -0400

    fix: cirrus search version

--- download_git_extensions.sh
+++ download_git_extensions.sh
@@ -3,7 +3,7 @@ set -e
 # You can specify extensions to be downloaded via GitHub here. You MUST specify a target folder below as well, or shit will break :)
 extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth wikimedia/mediawiki-extensions-Elastica wikimedia/mediawiki-extensions-CirrusSearch )
 targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth Elastica CirrusSearch )
-refs=( master master master master master master master REL1_46 master)
+refs=( master master master master master master master REL1_46 REL1_46 )
 
 for i in "${!extensions[@]}"; do
     extension=${extensions[$i]}
