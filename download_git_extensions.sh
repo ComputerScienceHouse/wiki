cc5b122a6c31324656356e6e30a3b6f62fb8d084
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-09-21 23:42:47 -0400

    feat: attempting to install cirrussearch

--- download_git_extensions.sh
+++ download_git_extensions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # You can specify extensions to be downloaded via GitHub here. You MUST specify a target folder below as well, or shit will break :)
-extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth wikimedia/mediawiki-extensions-Elastica )
-targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth Elastica )
-refs=( master master master master master master master REL1_46 )
+extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth wikimedia/mediawiki-extensions-Elastica wikimedia/mediawiki-extensions-CirrusSearch )
+targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth Elastica CirrusSearch )
+refs=( master master master master master master master REL1_46 master)
 
 for i in "${!extensions[@]}"; do
     extension=${extensions[$i]}
