329757ffa714c0fb3131662bc6c3ad2c98085384
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-09-21 20:55:55 -0400

    feat: install mediawiki

--- download_git_extensions.sh
+++ download_git_extensions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 # You can specify extensions to be downloaded via GitHub here. You MUST specify a target folder below as well, or shit will break :)
-extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth )
-targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth )
+extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth wikimedia/mediawiki-extensions-Elastica )
+targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth Elastica )
 
 for i in "${!extensions[@]}"; do
     extension=${extensions[$i]}
