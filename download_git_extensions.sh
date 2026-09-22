e910be632a1700ee2200421e65f331028d0e9905
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-09-21 21:17:31 -0400

    fix: right branch

--- download_git_extensions.sh
+++ download_git_extensions.sh
@@ -3,12 +3,15 @@ set -e
 # You can specify extensions to be downloaded via GitHub here. You MUST specify a target folder below as well, or shit will break :)
 extensions=( Telshin/Spoilers Pavelovich/WikiBanner wikimedia/mediawiki-extensions-OpenIDConnect wikimedia/mediawiki-extensions-DarkMode edwardspec/mediawiki-aws-s3 ComputerScienceHouse/wiki-user-sig wikimedia/mediawiki-extensions-PluggableAuth wikimedia/mediawiki-extensions-Elastica )
 targets=( Spoilers WikiBanner OpenIDConnect DarkMode AWS CSHUserSig PluggableAuth Elastica )
+refs=( master master master master master master master REL1_46 )
 
 for i in "${!extensions[@]}"; do
     extension=${extensions[$i]}
     target=${targets[$i]}
+    ref=${refs[$i]}
+
     echo Downloading $extension...
-    wget https://github.com/$extension/archive/master.zip
+    wget https://github.com/$extension/archive/$ref.zip
     unzip master.zip
     rm master.zip
 
